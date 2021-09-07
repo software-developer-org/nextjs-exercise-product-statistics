a50d62b50dfad56692b7c91f7764d1f27ff01bda
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2021-09-07 21:42:42 +0200

    style product as card

--- src/product.tsx
+++ src/product.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { ProductType } from '../lib/products';
-('../lib/products');
+import styles from './product.module.scss';
 
 type Props = {
   product: ProductType;
 };
 
 const Product: React.FC<Props> = ({ product }) => {
-  return <div>{product.description}</div>;
+  return <div className={styles.card}>{product.description}</div>;
 };
 export default Product;
