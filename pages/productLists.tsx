b7ffe5eb83b32f05e622f5c7f4e07c7eef376ada
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2021-09-08 13:10:23 +0200

    sort by price

--- pages/productLists.tsx
+++ pages/productLists.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@material-ui/core';
+import { Box, Container, FormControlLabel, Switch } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { GetStaticProps } from 'next';
@@ -20,17 +20,33 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const ProductLists: React.FC<Props> = ({ allProducts }) => {
+  const [priceAsc, setPriceAsc] = useState(true);
+  const sort = (p: ProductType[]) => {
+    return p.sort((a, b) => {
+      const priceA = a.priceR ? a.priceR : a.priceO;
+      const priceB = b.priceR ? b.priceR : b.priceO;
+      const sorted = priceAsc ? priceA - priceB : priceB - priceA;
+      return sorted;
+    });
+  };
+
   const [sizeFilter, setSizeFilter] = useState('');
   const initialProducts: ProductType[] = sizeFilter ? allProducts.filter((p) => p.sizes.filter((productSize) => productSize === sizeFilter).length > 0) : allProducts;
   const [products, setProducts] = useState(initialProducts);
 
-  const listItems = products.map((p) => <Product product={p} key={p.id} />);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const listItems = sort(products).map((p) => <Product product={p} key={p.id} />);
+
+  const handleFilterBySize = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newSizeFilterValue = e.target.value.toUpperCase();
     setSizeFilter(newSizeFilterValue);
     const productsBySize: ProductType[] = newSizeFilterValue ? allProducts.filter((p) => p.sizes.filter((productSize) => productSize === newSizeFilterValue).length > 0) : allProducts;
-    setProducts(productsBySize);
+    setProducts(sort(productsBySize));
+  };
+
+  const handleSort = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newVal = e.target.checked;
+    setPriceAsc(newVal);
   };
 
   return (
@@ -38,7 +54,8 @@ const ProductLists: React.FC<Props> = ({ allProducts }) => {
       <Typography variant="h4" component="h1" gutterBottom>
         Next.js example
       </Typography>
-      <TextField label="Größe" helperText="Nach Größe Filtern" type="text" value={sizeFilter} onChange={handleChange}></TextField>
+      <TextField label="Größe" helperText="Nach Größe Filtern" type="text" value={sizeFilter} onChange={handleFilterBySize}></TextField>
+      <FormControlLabel control={<Switch checked={priceAsc} onChange={handleSort}></Switch>} label={priceAsc ? 'Preise aufsteigend' : 'Preis absteigend'}></FormControlLabel>
       <Box display="flex" flexDirection="row" flexWrap="wrap">
         {listItems}
       </Box>
