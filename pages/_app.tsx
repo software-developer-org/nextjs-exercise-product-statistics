c39ff3ef294823272a3d8bf2527c39d36182d8fe
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2021-09-11 20:32:45 +0200

    reorganize imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import type { AppProps } from 'next/app'
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import React from 'react';
 import theme from '../src/theme';
 
 export default function MyApp(props: AppProps) {
