d035cd38b425ec8587da95d07c6bb1ce93453f6e
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2021-09-11 21:47:25 +0200

    test getLargestSelectionBySize()

--- __tests__/lib/statistics.ts
+++ __tests__/lib/statistics.ts
@@ -1,5 +1,5 @@
 import { ProductType } from '../../lib/products';
-import { getMostProductsByBrandsLessThan40 } from '../../lib/statistics';
+import { getLargestSelectionBySize, getMostProductsByBrandsLessThan40 } from '../../lib/statistics';
 import { randomInteger } from '../../__mocks__/fixture-utils';
 
 test('getMostProductsByBrandsLessThan40(): brand1 with two products', () => {
@@ -52,7 +52,7 @@ test('getMostProductsByBrandsLessThan40(): brand1 and brand 2 has same amount of
   const product4 = { brand: 'brand2', description: 'description4', id: randomInteger() + '', images: [], sizes: [], url: '', priceO: 50, priceR: 50 };
   const product5 = { brand: 'brand2', description: 'description5', id: randomInteger() + '', images: [], sizes: [], url: '', priceO: 50 };
   const product6 = { brand: 'brand2', description: 'description6', id: randomInteger() + '', images: [], sizes: [], url: '', priceO: 38 };
-  const product7 = { brand: 'brand2', description: 'description2', id: randomInteger() + '', images: [], sizes: [], url: '', priceO: 50, priceR: 0.50 };
+  const product7 = { brand: 'brand2', description: 'description2', id: randomInteger() + '', images: [], sizes: [], url: '', priceO: 50, priceR: 0.5 };
   const products: ProductType[] = [product1, product2, product3, product4, product5, product6, product7];
 
   //test
@@ -68,4 +68,44 @@ test('getMostProductsByBrandsLessThan40(): brand1 and brand 2 has same amount of
   expect(result[1].products.length).toEqual(2);
   expect(result[1].products[0]).toEqual(product6);
   expect(result[1].products[1]).toEqual(product7);
+});
+
+test('getLargestSelectionBySize(): brand1 with 4 sizes', () => {
+  // brand 1
+  const product1 = { brand: 'brand1', description: 'description1', id: randomInteger() + '', images: [], sizes: ['1', '2', '3'], url: '', priceO: 38 };
+  const product2 = { brand: 'brand1', description: 'description2', id: randomInteger() + '', images: [], sizes: ['2', '3'], url: '', priceO: 50, priceR: 39.99 };
+  const product3 = { brand: 'brand1', description: 'description3', id: randomInteger() + '', images: [], sizes: ['1', '4'], url: '', priceO: 40 };
+  // brand 2
+  const product4 = { brand: 'brand2', description: 'description4', id: randomInteger() + '', images: [], sizes: ['a', 'b'], url: '', priceO: 50, priceR: 50 };
+  const product5 = { brand: 'brand2', description: 'description5', id: randomInteger() + '', images: [], sizes: ['b'], url: '', priceO: 50 };
+  const product6 = { brand: 'brand2', description: 'description6', id: randomInteger() + '', images: [], sizes: ['c'], url: '', priceO: 38 };
+  const product7 = { brand: 'brand2', description: 'description2', id: randomInteger() + '', images: [], sizes: ['b', 'c'], url: '', priceO: 50, priceR: 0.5 };
+  const products: ProductType[] = [product1, product2, product3, product4, product5, product6, product7];
+  //test
+  const result = getLargestSelectionBySize(products);
+
+  expect(result.length).toEqual(1);
+  expect(result[0].brand).toEqual('brand1');
+  expect(result[0].sizes).toEqual(['1', '2', '3', '4']);
+});
+
+test('getLargestSelectionBySize(): brand1 and brand2 with 4 sizes', () => {
+  // brand 1
+  const product1 = { brand: 'brand1', description: 'description1', id: randomInteger() + '', images: [], sizes: ['1', '2', '3'], url: '', priceO: 38 };
+  const product2 = { brand: 'brand1', description: 'description2', id: randomInteger() + '', images: [], sizes: ['2', '3'], url: '', priceO: 50, priceR: 39.99 };
+  const product3 = { brand: 'brand1', description: 'description3', id: randomInteger() + '', images: [], sizes: ['1', '4'], url: '', priceO: 40 };
+  // brand 2
+  const product4 = { brand: 'brand2', description: 'description4', id: randomInteger() + '', images: [], sizes: ['a', 'b'], url: '', priceO: 50, priceR: 50 };
+  const product5 = { brand: 'brand2', description: 'description5', id: randomInteger() + '', images: [], sizes: ['b'], url: '', priceO: 50 };
+  const product6 = { brand: 'brand2', description: 'description6', id: randomInteger() + '', images: [], sizes: ['c'], url: '', priceO: 38 };
+  const product7 = { brand: 'brand2', description: 'description2', id: randomInteger() + '', images: [], sizes: ['d'], url: '', priceO: 50, priceR: 0.5 };
+  const products: ProductType[] = [product1, product2, product3, product4, product5, product6, product7];
+  //test
+  const result = getLargestSelectionBySize(products);
+
+  expect(result.length).toEqual(2);
+  expect(result[0].brand).toEqual('brand1');
+  expect(result[0].sizes).toEqual(['1', '2', '3', '4']);
+  expect(result[1].brand).toEqual('brand2');
+  expect(result[1].sizes).toEqual(['a', 'b', 'c', 'd']);
 });
